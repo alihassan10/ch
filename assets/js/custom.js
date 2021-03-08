23c60ecb8a00dce2b3a8d97445cacd6f358689dd
alihassan10 <neymarneymar1100@gmail.com>
2021-03-08 22:35:14 +0500

    ch1

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+$('#testimonial-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    center:true,
+    autoplay:true,
+    margin: 10,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
